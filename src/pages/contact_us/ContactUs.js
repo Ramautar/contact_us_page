0ac19e4f297c2a2473fc842f1f21e9aebb2e2666
Ramautar <r.ramautar@novi-education.nl>
2021-03-18 21:07:48 +0100

    aanpassing mobiele versie

--- src/pages/contact_us/ContactUs.js
+++ src/pages/contact_us/ContactUs.js
@@ -3,78 +3,73 @@ import './ContactUs.css';
 
 function ContactUs(){
     return(
-        <div className="container">
-            <header>
-                <h1>Contact Us</h1>
-                <p>
-                    Lorem ipsum dolor sit amet
-                    consectetur adipisicing elit.
-                    Nam totam iure vitae nesciunt quaerat sed, et ullam placeat ut?
-                    Impedit dolorem nostrum itaque molestias nemo delectus animi fuga illum
-                    magnam earum pariatur reiciendis, explicabo id velit facilis,
-                    necessitatibus cum! Magnam.
-                </p>
+        <div className='contact-us'>
+            <header className='contact-us-header'>
+                <h1>Contact us</h1>
+                <p>Willekeurige Contact Us Page!!! met fictief gegevens</p>
             </header>
-            <div className="content">
-                <div className="content-form">
+            <div className='contact-us-container'>
+                <div className='contact-us-left'>
                     <section>
-                        <i className="fa fa-map-marker fa-2x" aria-hidden="true"></i>
-                        <h2>address</h2>
+                        <i className="fa fa-map-marker fa-2x" aria-hidden="true" />
+                        <h2>Adres</h2>
                         <p>
-                            Lorem ipsum dolor sit. <br />
-                            Lorem, ipsum dolor. <br />
-                            lorem
+                            Mathenesserweg 78b <br />
+                            3078 HG Rotterdam<br />
+                            Zuid-Holland
                         </p>
                     </section>
-
                     <section>
-                        <i className="fa fa-phone fa-2x" aria-hidden="true"></i>
-                        <h2>Phone</h2>
-                        <p>123-456-78901548</p>
+                        <i className="fa fa-phone fa-2x" aria-hidden="true" />
+                        <h2>Telefoon nummer</h2>
+                        <p>06-1234567</p>
                     </section>
-
                     <section>
-                        <i className="fa fa-envelope fa-2x" aria-hidden="true"></i>
+                        <i className="fa fa-envelope fa-2x" aria-hidden="true" />
                         <h2>E-mail</h2>
-                        <p>dghdgqd@temporary.net</p>
+                        <p>onbekend@gmail.com</p>
                     </section>
+
                 </div>
-            </div>
+                <div className='contact-us-right'>
+                    <form>
+                        <div className="form">
+                            <div className="right">
 
-            <form>
-                <div className="form">
-                    <div className="right">
-                        <div className="contact-form">
-                            <input type="text" required />
-                            <span className='placeholder'>Full Name</span>
-                        </div>
+                                <div className="contact-form">
+                                    <input type="text" required />
+                                    <span className='placeholder'>Full Name</span>
+                                </div>
 
-                        <div className="contact-form">
-                            <input type="E-mail" required />
-                            <span className='placeholder'>E-mail Id</span>
-                        </div>
-                        <div className="contact-form">
-                            <textarea name="text">
-                            </textarea>
-                            <span className='placeholder'> Type your Message....</span>
-                        </div>
+                                <div className="contact-form">
+                                    <input type="E-mail" required />
+                                    <span className='placeholder'>E-mail Id</span>
+                                </div>
 
-                        <div className="contact-form">
-                            <input type="submit" name="submit" />
+                                <div className="contact-form">
+                                    <textarea name="text"> </textarea>
+                                    <span className='placeholder'> Type your Message....</span>
+                                </div>
+
+                                <div className="contact-form">
+                                    <input type="submit" name="submit" />
+                                </div>
+                            </div>
                         </div>
+                    </form>
+                    <div className="media">
+                        <li><i className="fa fa-facebook-square fa-2x" aria-hidden="true" /></li>
+                        <li><i className="fa fa-instagram fa-2x" aria-hidden="true" /></li>
+                        <li><i className="fa fa-whatsapp fa-2x" aria-hidden="true" /></li>
+                        <li><i className="fa fa-twitter-square fa-2x" aria-hidden="true" /></li>
                     </div>
                 </div>
-            </form>
-            <div className="media">
-                <li><i className="fa fa-facebook-square fa-2x" aria-hidden="true"> </i></li>
-                <li><i className="fa fa-instagram fa-2x" aria-hidden="true"> </i></li>
-                <li><i className="fa fa-whatsapp fa-2x" aria-hidden="true"> </i></li>
-                <li><i className="fa fa-twitter-square fa-2x" aria-hidden="true"> </i></li>
             </div>
             <div className="empty">
             </div>
         </div>
-      )
+    )
 }
 
 export default ContactUs;
+
